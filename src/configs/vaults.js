cf6087631af3d1e3dae8c747dae691cb6a7277c3
Tue Nguyen <tue.nguyen@madison-technology.com>
2022-02-22 11:51:48 +0700

    feat: vault-31338 eth vault

--- src/configs/vaults.js
+++ src/configs/vaults.js
@@ -2,6 +2,7 @@ import { default as vaults56 } from './vaults-56';
 import { default as vaults1 } from './vaults-1';
 import { default as vaults97 } from './vaults-97';
 import { default as vaults31340 } from './vaults-31340';
+import { default as vaults31338 } from './vaults-31338';
 
 
 const vaults = process.env.REACT_APP_PROPOSAL_NETWORK === 'mainnet' ?
@@ -27,6 +28,10 @@ const vaults = process.env.REACT_APP_PROPOSAL_NETWORK === 'mainnet' ?
     {
       id: 31340,
       list: vaults31340
+    },
+    {
+      id: 31338,
+      list: vaults31338
     }
   ];
 
